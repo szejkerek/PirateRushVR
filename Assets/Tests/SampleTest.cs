97fd1f965e9a326b9b38cdc9cac2a08dd0585bf3
Bartłomiej Gordon <bartekk.gordon@gmail.com>
2023-10-12 19:57:03 +0200

    Add sample test

--- Assets/Tests/SampleTest.cs
+++ Assets/Tests/SampleTest.cs
@@ -6,20 +6,9 @@ using UnityEngine.TestTools;
 
 public class SampleTest
 {
-    // A Test behaves as an ordinary method
     [Test]
     public void SampleTestSimplePasses()
     {
-        // Use the Assert class to test conditions
-    }
-
-    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    // `yield return null;` to skip a frame.
-    [UnityTest]
-    public IEnumerator SampleTestWithEnumeratorPasses()
-    {
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
-        yield return null;
+        Assert.AreEqual(1, 1);
     }
 }
