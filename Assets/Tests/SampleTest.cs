2aa5e66cce347c13d91e409b814c542addb546c2
Bartłomiej Gordon <bartekk.gordon@gmail.com>
2023-11-07 11:35:28 +0100

    Add Test for TickEngine

--- Assets/Tests/SampleTest.cs
+++ Assets/Tests/SampleTest.cs
@@ -1,14 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 
 public class SampleTest
 {
-    [Test]
-    public void SampleTestSimplePasses()
+    [UnityTest]
+    [TestCase(1, 1)]
+    [TestCase(2, 2)]
+    [TestCase(4, 4)]
+    public IEnumerator SampleTestSimplePasses(int tickRate, int expectedTickCount)
     {
-        Assert.AreEqual(1, 1);
+        int tickCount = 0;
+        TickEngine tickEngine = new TickEngine(tickRate);
+
+        tickEngine.OnTick += () => tickCount++;
+
+        float elapsedTime = 0;
+        while (elapsedTime < 1f)
+        {
+            tickEngine.UpdateTicks(Time.deltaTime);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        Assert.AreEqual(expectedTickCount, tickCount);
     }
 }
