6a7372a242accaa79d8f43121fc9eae94e722d92
Bartłomiej Gordon <bartekk.gordon@gmail.com>
2023-12-05 15:15:10 +0100

    Start global settings

---
+++ Assets/__Scripts/Data/GlobalSettings.cs
@@ -0,0 +1,10 @@
+using System;
+
+[Serializable]
+public class GlobalSettings
+{
+    public string LastNickname;
+    public float Volume;
+    public string TurnType;
+    public bool UsingVigniete;
+}
