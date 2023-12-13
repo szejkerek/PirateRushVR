9a3bb6a6eb5f5433be0c3a9a88dfd5c629abf009
Bartłomiej Gordon <bartekk.gordon@gmail.com>
2023-12-13 14:11:26 +0100

    Add cannon poof sound

--- Assets/__Scripts/Cannons/CannonShooting.cs
+++ Assets/__Scripts/Cannons/CannonShooting.cs
@@ -6,6 +6,7 @@ public class CannonShooting : MonoBehaviour
     [SerializeField] CannonSettings settings;
 
     //[SerializeField] private GameObject cannonSmoke;
+    [SerializeField] private Sound shootingSound;
     [SerializeField] private Transform shootingPoint;
 
     Transform target;
@@ -45,6 +46,7 @@ public class CannonShooting : MonoBehaviour
 
         InitProjectile(projectile, targetDirection, gravity);
 
+        AudioManager.Instance.Play(gameObject, shootingSound, SoundType.SFX);
         //GameObject effect = Instantiate(cannonSmoke, shootingPoint.transform.position, Quaternion.LookRotation(shootingPoint.transform.forward));
         //Destroy(effect, 5f);
     }
